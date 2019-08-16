85ab700b71f20228d576a26c113fc98fef45912f
Andres Sarmiento Tobon <a.sarmientot@uniandes.edu.co>
2019-08-16 16:44:13 -0500

    Agregar funciones de herederos y de transferencia a otras cuentas

--- contracts/Marranito.sol
+++ contracts/Marranito.sol
@@ -2,15 +2,33 @@ pragma solidity >= 0.4.23 <0.6.0;
 
 contract Marranito {
   address owner;
+  uint8 totalPercentage;
+  mapping(address => uint8) heirsToPercentage;
 
   event funds(address _sender, uint _value, string _msg);
 
   constructor() public {
     owner = msg.sender;
+    totalPercentage = 0;
   }
 
   modifier onlyOwner() {
     require(msg.sender == owner, "You are not the owner");
+    _;
+  }
+
+  modifier heirDoesntExists(address _heir) {
+    require(heirsToPercentage[_heir] == uint8(0x0), "This heir already exists");
+    _;
+  }
+
+  modifier percentagePositive(uint8 _percentage) {
+    require(_percentage > 0, "This percentage is not positive");
+    _;
+  }
+
+  modifier percentageLsEq100(uint8 _percentage) {
+    require(_percentage <= 100, "This percentage is bigger than 100");
     _;
   }
 
@@ -24,6 +42,42 @@ contract Marranito {
 
   function transfer(address _newOwner) public onlyOwner {
     owner = _newOwner;
+  }
+
+  function sendFunds(address payable _to, uint value) public onlyOwner {
+     _to.transfer(value);
+  }
+
+  function hasHeir(address payable _heir) public view returns(bool) {
+    return heirsToPercentage[_heir] != 0;
+  }
+
+  function getHeirPercentage(address payable _heir) public view returns(uint8) {
+    return heirsToPercentage[_heir];
+  }
+
+  function addHeir(address payable _heir, uint8 _percentage) public heirDoesntExists(_heir)
+      percentagePositive(_percentage) percentageLsEq100(_percentage) onlyOwner returns(bool) {
+    updateHeir(_heir, _percentage);
+    return heirsToPercentage[_heir] == 0;
+  }
+
+  function updateHeir(address payable _heir, uint8 _percentage) public percentagePositive(_percentage)
+      percentageLsEq100(_percentage) onlyOwner returns(uint8) {
+    return _updateHeir(_heir, _percentage);
+  }
+
+  function removeHeir(address payable _heir) public onlyOwner returns(bool) {
+    _updateHeir(_heir, -heirsToPercentage[_heir]);
+    heirsToPercentage[_heir] = uint8(0x0);
+    return heirsToPercentage[_heir] == uint8(0x0);
+  }
+
+  function _updateHeir(address payable _heir, uint8 _percentage) private returns(uint8) {
+    uint8 _currentPercentage = heirsToPercentage[_heir];
+    totalPercentage += _percentage;
+    heirsToPercentage[_heir] = _percentage;
+    return _currentPercentage;
   }
 
   function() external payable {}
--- test/01-marranito.js
+++ test/01-marranito.js
@@ -12,6 +12,7 @@ contract('Marranito', accounts => {
     owner,
     unauthorized,
     userA,
+    userB
   ] = accounts;
   describe('Deployment', () => {
     it('should deploy the contract without problems', () => {
@@ -70,6 +71,73 @@ contract('Marranito', accounts => {
     it('should not allow not owner to seize the contract', () => {
       return expect(marranito.transfer(owner, {from: owner}))
         .to.be.eventually.rejected;
+    });
+    it('should send funds to other addresses', () => {
+      return expect(marranito.sendFunds(unauthorized, web3.utils.toWei('1'), {from: userA}))
+        .to.be.eventually.fulfilled;
+    });
+    it('should only owner to send funds to other addresses', () => {
+      return expect(marranito.sendFunds(userB, web3.utils.toWei('1'), {from: unauthorized}))
+        .to.be.eventually.rejected;
+    });
+    it('should allow to add an heir to the owner', () => {
+      return marranito.addHeir(owner, 10, {from: userA}).then(response => {
+        marranito.getHeirPercentage(owner).then(result => {
+          expect(result.toString(10)).to.be.eq('10');
+        });
+      });
+    });
+    it('should not allow to add an heir when heir already exists', () => {
+      return expect(marranito.addHeir(owner, 10, {from: userA}))
+        .to.be.eventually.rejected;
+    });
+    it('should not allow to add an heir not to the owner', () => {
+      return expect(marranito.addHeir(owner, 10, {from: unauthorized}))
+        .to.be.eventually.rejected;
+    });
+    it('should not allow to add an heir when percentage is <= 0', () => {
+      return expect(marranito.addHeir(userB, 0, {from: userA}))
+        .to.be.eventually.rejected;
+    });
+    it('should not allow to add an heir when percentage is > 100', () => {
+      return expect(marranito.addHeir(userB, 101, {from: userA}))
+        .to.be.eventually.rejected;
+    });
+    it('should allow to remove an heir to the owner', () => {
+      return marranito.removeHeir(owner, {from: userA}).then(response => {
+        expect(marranito.hasHeir(owner)).to.be.eventually.false;
+      });
+    });
+    it('should not allow to remove an heir not to the owner', () => {
+      return expect(marranito.removeHeir(owner, {from: unauthorized}))
+        .to.be.eventually.rejected;
+    });
+    it('should allow to update an heir to the owner', () => {
+      return marranito.updateHeir(owner, 15, {from: userA}).then(response => {
+        marranito.getHeirPercentage(owner).then(result => {
+          expect(result.toString(10)).to.be.eq('15');
+        });
+      });
+    });
+    it('should not allow to update an heir not to the owner', () => {
+      return expect(marranito.updateHeir(owner, 15, {from: unauthorized}))
+        .to.be.eventually.rejected;
+    });
+    it('should not allow to add an heir when percentage is <= 0', () => {
+      return expect(marranito.addHeir(owner, 0, {from: userA}))
+        .to.be.eventually.rejected;
+    });
+    it('should not allow to add an heir when percentage is > 100', () => {
+      return expect(marranito.addHeir(owner, 101, {from: userA}))
+        .to.be.eventually.rejected;
+    });
+    it('should allow to prove an heir that they are one', () => {
+      return expect(marranito.hasHeir(owner, {from: unauthorized}))
+        .to.be.eventually.true;
+    });
+    it('should allow to prove an heir that they are one', () => {
+      return expect(marranito.hasHeir(unauthorized, {from: unauthorized}))
+        .to.be.eventually.false;
     });
   });
 });
